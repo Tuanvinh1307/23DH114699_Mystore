7eb6e90b31d3953f2844a73c73b9d3c9d659eee2
SV <SV@PM19-PC14>
2024-10-14 08:25:39 +0700

    Add project files.

---
+++ 23DH114699_Mystore/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace _23DH114699_Mystore.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional},
+                new[] { "_23DH114699_Mystore.Areas.Admin.Controllers" }
+
+            );
+        }
+    }
+}
---
+++ 23DH114699_Mystore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH114699_Mystore.MvcApplication" Language="C#" %>
